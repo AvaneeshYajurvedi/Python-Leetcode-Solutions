db920fd2e63560ec4a7942d1884ebed9ee9d0802
Avaneesh Yajurvedi <aviiy1710@gmail.com>
2026-01-31 12:51:15 +0530

    Add Majority Element solution in Python

---
+++ HashTable/Majority_element.py
@@ -0,0 +1,16 @@
+'''Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.'''
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count=0
+        c=None
+        for num in nums:
+            if count==0:
+                c=num
+            if num==c:
+                count+=1
+            else:
+                count-=1
+        return c
